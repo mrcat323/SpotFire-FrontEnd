c2d87878239c12a39a26c3b204091c9a22e73ec6
Mr. CaT <mrcat323@gmail.com>
2020-10-02 07:10:51 -0400

    fixed a typo

--- src/views/About.vue
+++ src/views/About.vue
@@ -3,7 +3,7 @@
     <h3 class="title">Who are we ?</h3>  
     <v-spacer></v-spacer>
     <v-img class="centered-image" alt="Cosmo Spirit LOGO" :lazy-src="logo" :src="logo" max-width="300" max-height="300" />  
-    <p class="subtitle-1">We are <span class="font-weight-bold">Cosmo Spirit</span> the  and we make the world better providing spotting wildfires with our technology</p>
+    <p class="subtitle-1">We are <span class="font-weight-bold">The Cosmo Spirit</span> and we make the world better providing spotting wildfires with our technology</p>
 
   </div>
 </template>
