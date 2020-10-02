ef9a6cdbe2587bba86c21580021a1cb95628ee85
Mr. CaT <mrcat323@gmail.com>
2020-10-02 07:06:50 -0400

    "About" and "Home" pages are here

---
+++ src/views/About.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="about">
+    <h3 class="title">Who are we ?</h3>  
+    <v-spacer></v-spacer>
+    <v-img class="centered-image" alt="Cosmo Spirit LOGO" :lazy-src="logo" :src="logo" max-width="300" max-height="300" />  
+    <p class="subtitle-1">We are <span class="font-weight-bold">Cosmo Spirit</span> the  and we make the world better providing spotting wildfires with our technology</p>
+
+  </div>
+</template>
+<script>
+export default {
+  computed: {
+    logo() {
+      return require('../assets/logo.png')  
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+
+.centered-image {
+  margin: 0 auto;
+}
+
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-container>
+    <v-row align="center">
+    <v-card class="mx-auto my-12" max-width="350">
+      <v-card-title>
+        Welcome
+      </v-card-title>
+      <v-card-text>
+        <v-row>
+          <p class="font-italic">
+            Welcome to our application! We do help to find wildfires with our neural network.  
+          </p>
+        </v-row>
+      </v-card-text>
+    </v-card>    
+    </v-row>  
+      
+  </v-container>
+</template>
