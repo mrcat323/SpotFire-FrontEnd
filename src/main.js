9f07c94c1596a2ed2185c30afd4e54d9ec7abace
Mr. CaT <mrcat323@gmail.com>
2020-10-03 16:07:01 -0400

    new changes
    - removed the vuetify
    - added buefy instead
    - material icons connected

--- src/main.js
+++ src/main.js
@@ -1,12 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
-import vuetify from './plugins/vuetify';
 import router from './router'
+import Buefy from 'buefy'
+import 'buefy/dist/buefy.css'
+import '@mdi/font/css/materialdesignicons.min.css'
+
+Vue.use(Buefy)
 
 Vue.config.productionTip = false
 
 new Vue({
-  vuetify,
   router,
   render: h => h(App)
 }).$mount('#app')
