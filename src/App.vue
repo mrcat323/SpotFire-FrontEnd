b98ccaecb1692cf9fb31af8158f627680b72825e
Mr. CaT <mrcat323@gmail.com>
2020-10-03 16:06:33 -0400

    cleared out the main component

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,9 @@
 <template>
-  <main>
-    <v-app>
       <div id="app">
-        <div id="nav">
-          <router-link to="/">Home</router-link> |
-          <router-link to="/about">About</router-link> |
-          <router-link to="/login">Login</router-link> |
-          <router-link to="/register">Register</router-link>
-        </div>
         <router-view/>
       </div>  
-    </v-app>
-  </main>
 </template>
-
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>
+<script>
+/* eslint-disable */
+export default {}
+</script>
