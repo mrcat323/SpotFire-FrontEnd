cc7e6ce3e6e7f2cd0a1caf71eddd788186260e74
Mr. CaT <mrcat323@gmail.com>
2020-10-04 13:48:28 -0400

    Footer had been added

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,22 @@
 <template>
       <div id="app">
         <router-view/>
+        <footer class="footer mt-50">
+          <div class="content has-text-centered">
+            <p>
+              <strong>Spotfire</strong> by <a href="https://2020.spaceappschallenge.org/challenges/confront/spot-fire-3/teams/cosmospirit">CosmoSpirit</a>. The source code is licensed
+              <a href="https://opensource.org/licenses/GPL-2.0">GPL v2</a>
+            </p>
+          </div>
+      </footer>
       </div>  
 </template>
 <script>
 /* eslint-disable */
 export default {}
 </script>
+<style lang="scss" scoped>
+.mt-50 {
+  margin-top: 75px;  
+}
+</style>
