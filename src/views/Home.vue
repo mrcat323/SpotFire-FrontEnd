4619dbce1cb9a808b29afc6fb5a8538e956a0dd3
Mr. CaT <mrcat323@gmail.com>
2020-10-04 07:18:38 -0400

    Main stuff is done, mate

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,27 +1,29 @@
 <template>
   <div>
 	  <Navbar :bg-image="img" title="Welcome to CosmoSpirit" sub-title="SpaceApps team that prevents wildfires" />
-  <div class="container pt-25">
+  <div class="container pt-25 mb-75">
 		<section class="section">
 			<div class="content">
 				<div class="columns is-centered">
 					<div class="column is-half">
 						<b-field message="What do you want to search?">
 							<b-input placeholder="Search..."
+								v-model="city"
 								type="search"
-								icon="magnify">
+								icon="magnify"
+								minlength="3">
 							</b-input>
 							<p class="control">
-								<button class="button is-primary"><i class="mdi mdi-magnify"></i></button>
+								<button @click="grab" class="button is-primary"><i class="mdi mdi-magnify"></i></button>
 							</p>
 						</b-field>	
 					</div>
 					<div class="column is-half">
-						<div class="card">
+						<div class="card" v-if="information">
 							<div class="card-content">
 								<div class="media">
 									<div class="media-content">
-										<p class="title is-4">Dushanbe</p>
+										<p class="title is-4">{{ information.city }}</p>
 									</div>
 								</div>
 
@@ -29,22 +31,27 @@
 									<p class="subtitle">
 									<b-icon icon="weather-sunny" size="is-medium">
 									</b-icon>		
-									28 &#8451;</p>
+									{{ information.temp }} &#8451;</p>
 									<p class="subtitle">
 									<b-icon
 									icon="water"
 									size="is-medium"
 									>
 									</b-icon>	
-									Dry</p>
+									{{ information.humidity }}%</p>
+									<p class="subtitle">
+									<b-icon
+									icon="fire"
+									size="is-medium"
+									></b-icon>	
+									{{ information.fire }}%</p>
 									<p class="subtitle">
 									<b-icon
 									icon="wind-turbine"
 									size="is-medium"
 									></b-icon>	
-									1M/S</p>
+									{{ information.wind }}M/S</p>
 									<br>
-									<time datetime="2020-10-4">13:13PM - 04 Oct 2020</time>
 								</div>
 							</div>
 				</div>
@@ -57,11 +64,39 @@
 </template>
 <script>
 import Navbar from '../components/Navbar'
+import { SnackbarProgrammatic as Snackbar } from 'buefy'
+import axios from 'axios'
+
+axios.defaults.baseURL = 'http://fire.rogo.tj'
 
 export default {
 	data: () => ({
+		city: '',
+		error: '',
+		information: null,		
 		img: 'https://media-public.canva.com/MADQ5PdZ0-A/1/screen_2x.jpg'
 	}),
+	methods: {
+		grab() {
+			if (this.city.length) {
+				axios.get(`/city/${this.city}`)
+				.then(res => {
+					this.information = res.data.city_info
+				}).catch(err => {
+					console.log(err.response.data.status)	
+					if (err.response.status) {
+						this.error = 'Sorry, but we cannot find city in our database, try anything else'
+						Snackbar.open({ message: this.error, position: 'is-bottom-left'})
+					}
+				})	
+			}
+		}	
+	},
 	components: { Navbar }
 }
 </script>
+<style lang="scss" scoped>
+.mb-75 {
+	margin-bottom: 75px;	
+}
+</style>
