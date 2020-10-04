62c52fe47ff908815aea8d0702a42a7d4ad1ec68
Mr. CaT <mrcat323@gmail.com>
2020-10-04 06:26:09 -0400

    have no idea why it's here

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+{
+    loaders: [ 
+      {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        loaders: [
+          'file-loader?hash=sha512&digest=hex&name=[hash].[ext]',
+          'image-webpack-loader?bypassOnDebug&optimizationLevel=7&interlaced=false'
+        ]
+      } 
+    ]
+  }
