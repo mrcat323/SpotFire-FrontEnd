ad7bc5ae2f2a35f46f74ab93f630fc8a0a319999
Mr. CaT <mrcat323@gmail.com>
2020-10-04 06:26:46 -0400

    all about project, members

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,23 +1,291 @@
 <template>
-  <div class="container pt-30">
-    <h3 class="title">Who are we ?</h3>  
-    <img class="centered-image" alt="Cosmo Spirit LOGO" :src="logo" width="300" height="300" />  
-    <p class="subtitle-1">We are <span class="font-weight-bold">The Cosmo Spirit</span> and we make the world better providing spotting wildfires with our technology</p>
+  <div>
+    <Navbar :bg-image="img" title="About" sub-title="Project and team itself" />  
+    <div class="container pt-30">
+      <section class="section">
+        <div class="content">
+          <div class="columns is-centered">
+            <div class="column is-full">
+              <h1 class="title is-1 has-text-centered">About the project</h1>  
+              <p class="subtitle is-4 mt-5">
+                CosmoFire is global ecological project, which does solve one of important problems of mankind, "fire". We provide technology which prevents firings in certain places. During the computation process about chances of fire in certain place, also information about weather is going to be provided. Now project can be added to the Fire Department's system, giving a chance of being notified earlier than the fire can occur and prevent it. 
+                Now we do have information of 8.6k+ cities of all over the globe available.
+              </p>
+            </div>  
+          </div>
+        </div>
+      </section>  
+      <div class="container">
+      <div class="section">
+        <div class="card is-horizontal columns">
+          <div class="card-image column is-three-fifths">
+
+            <figure class="image is-4by3">
+              <img :src="daler" alt="this used to be photo">             
+            </figure>
+          </div>
+          <div class="card-content column is-two-fifths">
+            <div class="media">
+              <div class="media-content">
+                <p class="title is-4">Daler Hojimatov</p>
+                <p class="subtitle is-6">@ETH_Daler</p>
+              </div>
+            </div>
+
+            <div class="content">
+              This guy did well by inspiring other team-mates with his idea, With neural network, belief, capability of working together and build something which can change the world. His AI did a great work by grabbing information about chances of either fire and weather.<br><br>
+              <span class="tag is-link">Role</span> Back-end Developer <br>
+              <span class="tag is-info">By nature</span>: Back-end Developer, AI
+
+              <br><br>
+
+                <div class="field is-grouped is-grouped-multiline">
+                  <div class="control">
+                    <div class="tags has-addons">
+                      <span class="tag is-link">birthdate</span>
+                      <span class="tag is-info">07. 06. 2003</span>
+                    </div>
+                  </div>
+                  <br>
+                  <time datetime="2020-10-4">04 Oct 2020</time>
+
+                </div>
+            </div>
+          </div>
+        </div>
+  </div>
+        </div>
+        <div class="container">
+      <div class="section">
+        <div class="card is-horizontal columns">
+          <div class="card-image column is-three-fifths">
+
+            <figure class="image is-4by3">
+              <img :src="abdurahmon" alt="this used to be photo">             
+            </figure>
+          </div>
+          <div class="card-content column is-two-fifths">
+            <div class="media">
+              <div class="media-content">
+                <p class="title is-4">Hasanov Abdurahmon</p>
+                <p class="subtitle is-6">@mrcat323</p>
+              </div>
+            </div>
+
+            <div class="content">
+              Co-Inspirer, with skills of team-work, patience, and listening other team-mates' opinions worked on the front-end of the website, made it more dynamic and made it to work as other human-beings would expect.<br><br>
+              <span class="tag is-link">Role</span> Front-end Developer <br>
+              <span class="tag is-info">By nature</span>: Fullstack Web Developer, Open Source Activist
+
+              <br><br>
+
+                <div class="field is-grouped is-grouped-multiline">
+                  <div class="control">
+                    <div class="tags has-addons">
+                      <span class="tag is-link">birthdate</span>
+                      <span class="tag is-info">01. 02. 2001</span>
+                    </div>
+                  </div>
+                  <br>
+                  <time datetime="2018-5-8">15:51 PM - 28 May 2018</time>
+
+                </div>
+            </div>
+          </div>
+        </div>
+  </div>
+        </div>
+    
+        <div class="container">
+      <div class="section">
+        <div class="card is-horizontal columns">
+          <div class="card-image column is-three-fifths">
+
+            <figure class="image is-4by3">
+              <img :src="daler2" alt="Daler Safarov">             
+            </figure>
+          </div>
+          <div class="card-content column is-two-fifths">
+            <div class="media">
+              <div class="media-content">
+                <p class="title is-4">Daler Safarov</p>
+                <p class="subtitle is-6">@safarovdaler</p>
+              </div>
+            </div>
+
+            <div class="content">
+              This guy is cool <br>
+              <span class="tag is-link">Role</span> Motion-maker, Video-maker <br>
+              <span class="tag is-info">By nature</span>: Motion-maker, Video-maker
+
+              <br><br>
+
+                 <div class="field is-grouped is-grouped-multiline">
+                  <div class="control">
+                    <div class="tags has-addons">
+                      <span class="tag is-link">birthdate</span>
+                      <span class="tag is-info">29. 10. 2003</span>
+                    </div>
+                  </div>
+                  <br>
+                  <time datetime="2020-10-4">04 Oct 2020</time>
+
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+  </div>
+        
+        <div class="container">
+      <div class="section">
+        <div class="card is-horizontal columns">
+          <div class="card-image column is-three-fifths">
+
+            <figure class="image is-4by3">
+              <img :src="shohin" alt="Shohin Gulomuhidinov">             
+            </figure>
+          </div>
+          <div class="card-content column is-two-fifths">
+            <div class="media">
+              <div class="media-content">
+                <p class="title is-4">Shohin Gulomuhidinov</p>
+                <p class="subtitle is-6">@shoh1n</p>
+              </div>
+            </div>
+
+            <div class="content">
+              This guy is cool <br>
+              <span class="tag is-link">Role</span> Designer, Video-maker <br>
+              <span class="tag is-info">By nature</span>: Designer, Back-end Developer
+
+              <br><br>
+
+                <div class="field is-grouped is-grouped-multiline">
+                  <div class="control">
+                    <div class="tags has-addons">
+                      <span class="tag is-link">birthdate</span>
+                      <span class="tag is-info">21. 07. 2004</span>
+                    </div>
+                  </div>
+                  <br>
+                  <time datetime="2020-10-4">04 Oct 2020</time>
+
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+  </div>
+     <div class="container">
+      <div class="section">
+        <div class="card is-horizontal columns">
+          <div class="card-image column is-three-fifths">
+
+            <figure class="image is-4by3">
+              <img :src="jasur" alt="Jasur Marufov">             
+            </figure>
+          </div>
+          <div class="card-content column is-two-fifths">
+            <div class="media">
+              <div class="media-content">
+                <p class="title is-4">Jasur Marufov</p>
+                <p class="subtitle is-6">@littmrv</p>
+              </div>
+            </div>
+
+            <div class="content">
+              This guy is cool  <br>
+              <span class="tag is-link">Role</span> Front-end Developer <br>
+              <span class="tag is-info">By nature</span>: Front-end Developer
+
+              <br><br>
+
+              <div class="field is-grouped is-grouped-multiline">
+                <div class="field is-grouped is-grouped-multiline">
+                  <div class="control">
+                    <div class="tags has-addons">
+                      <span class="tag is-link">birthdate</span>
+                      <span class="tag is-info">01. 02. 2006</span>
+                    </div>
+                  </div>
+                  <br>
+                  <time datetime="2020-10-4">04 Oct 2020</time>
+
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+  </div>
+        </div>    
+  </div>  
   </div>
 </template>
 <script>
+/* eslint-disable */
+import Navbar from '../components/Navbar'
+
 export default {
+  data: () => ({
+    img: 'https://media-public.canva.com/orSXs/MADyQ2orSXs/1/s3.jpg'  
+  }),
+  components: { Navbar },
   computed: {
+    abdurahmon() {
+      return require('../assets/members/Abdurahmon.jpeg')  
+    },
+    daler() {
+      return require('../assets/members/Daler2.jpeg')  
+    },
+    daler2() {
+      return require('../assets/members/Daler.jpeg')  
+    },
+    shohin() {
+      return require('../assets/members/Shohin.jpeg')  
+    },
+    jasur() {
+      return require('../assets/members/Jasur.jpeg')  
+    },
     logo() {
       return require('../assets/logo.png')  
     }
   }
 }
 </script>
-<style lang="scss" scoped>
+<style lang="sass" scoped>
 
-.centered-image {
-  margin: 0 auto;
-}
+.centered-image 
+  margin: 0 auto
 
+.card
+  .media:not(:last-child)
+    margin-bottom: 0.75rem
+    
+  // Flex content order putting `.card-image` on right /* NOTE: Add this line */
+  &.card--reverse-order /* NOTE: Add this line */
+    flex-direction: row-reverse /* NOTE: Add this line */
+
+  &.is-horizontal
+    // flex-direction: row /* NOTE: Remove this line */
+    display: flex
+    flex-basis: 50ex
+    flex-grow: 0
+    flex-shrink: 1
+    .card-image
+      flex: 4
+      flex-shrink: 1
+      .image
+        display: block
+        position: relative
+        overflow: hidden
+        height: 100%
+        width: 100%
+        img
+          height: 100%
+          object-fit: cover
+          object-position: center
+
+    .card-content
+      flex: 3
 </style>
